a4a0d4d79e65c49999e5daf275bd62e5900bebed
AT COMPUTER <AT COMPUTER@ATT>
2024-02-03 19:03:22 +0600

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "We are from WADA-58 batch";
+            var output = data.Split(' ');
+            for(int i = 0; i < output.Count(); i++)
+            {
+                Console.WriteLine($"{i+1}.{output[i]}");
+            }
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+
+            }
+            string s = "https://atcomputer.net.bd/";
+            var outpu1t = s.Split('/');
+            for (int i = 0; i < outpu1t.Count(); i++)
+            {
+                Console.WriteLine($"{i + 1}.{outpu1t[i]}");
+            }
+             
+            Console.ReadKey();
+        }
+    }
+}
